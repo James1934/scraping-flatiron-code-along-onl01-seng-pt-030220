18eaf494e7d7677616b2d9a60e56a76def151dc6
Bartholomew Fountain <baertholomew@gmail.com>
2020-04-15 14:49:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,3 +18,14 @@ class Scraper
   
 end
 
+class Scraper
+ 
+  def get_page
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
+  end
+ 
+end
+ 
+Scraper.new.get_page
